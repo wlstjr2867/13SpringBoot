a1507ebe7413eb7a1dc63063ee8622f01061dd3e
wlstjr2867 <wlstjr2867@naver.com>
2025-07-25 18:25:06 +0900

    7월 25 학원 커어밋

--- B10aFileUpload/src/main/java/utils/MyFunctions.java
+++ B10aFileUpload/src/main/java/utils/MyFunctions.java
@@ -17,17 +17,23 @@ public class MyFunctions {
 		return uuid;
 	}
 	
+	//파일명 변경
 	public static String renameFile(String sDirectory,
 			String fileName) {
+		//파일의 확장자를 파일명의 끝에서부터 잘라낸다.
 		String ext = fileName.substring(fileName.lastIndexOf("."));
+		//UUID를 통해 파일명으로 사용할 문자열을 얻어온다.
 		String now = getUuid();
+		//둘을 합쳐서 새로운 파일명을 만든다.
 		String newFileName = now + ext;
 		
+		//File인스턴스를 생성한 후 파일명을 변경한다.
 		File oldFile =
 				new File(sDirectory + File.separator + fileName);
 		File newFile =
 				new File(sDirectory + File.separator + newFileName);
 		 oldFile.renameTo(newFile);
+		 //변경된 파일명을 반환한다.
 		 return newFileName;
 	}
 }
