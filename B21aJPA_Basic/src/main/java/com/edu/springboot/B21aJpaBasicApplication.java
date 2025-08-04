0f43bae249005de07017f2599730ef7b274f9ff4
wlstjr2867 <wlstjr2867@naver.com>
2025-08-04 13:17:37 +0900

    0804 커밋

---
+++ B21aJPA_Basic/src/main/java/com/edu/springboot/B21aJpaBasicApplication.java
@@ -0,0 +1,13 @@
+package com.edu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B21aJpaBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B21aJpaBasicApplication.class, args);
+	}
+
+}
