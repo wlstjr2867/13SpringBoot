76982a15c9d67140dc9130f182cae3878a5b5be4
wlstjr2867 <wlstjr2867@naver.com>
2025-08-05 13:11:10 +0900

    추가수정끝

---
+++ B21cSpringDataJPA2/src/main/java/com/edu/springboot/B21cSpringDataJpa2Application.java
@@ -0,0 +1,13 @@
+package com.edu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B21cSpringDataJpa2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B21cSpringDataJpa2Application.class, args);
+	}
+
+}
